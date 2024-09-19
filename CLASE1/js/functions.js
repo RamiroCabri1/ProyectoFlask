392d2f29af8e575c8455f0f80c32aad751028ea8
RamiroCabri <cabriramiro@gmail.com>
2024-09-19 15:03:41 -0300

    enviando

---
+++ CLASE1/js/functions.js
@@ -0,0 +1,17 @@
+function crearPersona(){
+
+    const nombre = document.getElementById("nombre").value;
+    const apellido = document.getElementById("apellido").value;
+    const dni = document.getElementById("dni").value;
+    const email = document.getElementById("email").value;
+
+    const nuevaPersona = {
+
+        name: nombre ,
+        surname: apellido,
+        dni: dni,
+        email: email
+
+    }
+
+}
